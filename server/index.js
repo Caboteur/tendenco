d8a42b06c920da0ab1a27500920c5dcd67ba091d
Adrien BLANCHOT <caboteur.adrienbanchot@gmail.com>
2020-04-21 17:17:15 +0200

    ok

--- server/index.js
+++ server/index.js
@@ -7,10 +7,8 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3001;
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
@@ -52,7 +50,7 @@ function googleCall(coutry) {
 
       });
 
-      app.get('/google-trend', (req, res) => res.send(results))
+
 
     }
   });
