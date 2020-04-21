fe95128156b9adddabd1ad57fdb41c1efca4bcc8
Adrien BLANCHOT <caboteur.adrienbanchot@gmail.com>
2020-04-21 16:52:38 +0200

    build

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
