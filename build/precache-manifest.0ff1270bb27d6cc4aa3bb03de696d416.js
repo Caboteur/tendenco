3ea027190b954b416c1ccb5bd69ed7cb35da9230
Adrien BLANCHOT <caboteur.adrienbanchot@gmail.com>
2020-04-21 14:42:09 +0200

    build

---
+++ build/precache-manifest.0ff1270bb27d6cc4aa3bb03de696d416.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399d9409ad701d92ae0150d746d3bdaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b802e297a85a9ac1b7e6",
+    "url": "/static/css/main.200461a0.chunk.css"
+  },
+  {
+    "revision": "c9e06256c930e41789af",
+    "url": "/static/js/2.abc24d1f.chunk.js"
+  },
+  {
+    "revision": "cc224d6948bc7bac74718dd43dd466c2",
+    "url": "/static/js/2.abc24d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b802e297a85a9ac1b7e6",
+    "url": "/static/js/main.26b9f0eb.chunk.js"
+  },
+  {
+    "revision": "4c0cb0e2145eb8a941a3",
+    "url": "/static/js/runtime-main.f1a3eaab.js"
+  },
+  {
+    "revision": "07c51e088dd888466d2688c5658a6d33",
+    "url": "/static/media/acaslonpro-regular.07c51e08.otf"
+  },
+  {
+    "revision": "486104b5a46aa7d49f1febbd98588c86",
+    "url": "/static/media/exit.486104b5.svg"
+  },
+  {
+    "revision": "a2cfb3a79e6343965ff18d42b71428b6",
+    "url": "/static/media/google.a2cfb3a7.svg"
+  },
+  {
+    "revision": "9922e8882ab58bac9eee74f9d952e24b",
+    "url": "/static/media/logo.9922e888.svg"
+  },
+  {
+    "revision": "40803abb3d7d631f5d5481db428c5000",
+    "url": "/static/media/news.40803abb.svg"
+  },
+  {
+    "revision": "e7c36e58412415e48423ef0c133496d3",
+    "url": "/static/media/twitter.e7c36e58.svg"
+  },
+  {
+    "revision": "bd7a7a1547a56d7111ba2dd9cafbe523",
+    "url": "/static/media/wikipedia.bd7a7a15.svg"
+  },
+  {
+    "revision": "728e38d904616d8c335b7dcd21d38485",
+    "url": "/static/media/youtube.728e38d9.svg"
+  }
+]);
