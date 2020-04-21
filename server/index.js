1934cea381ef9afb6f14fe254b8afd9463a7085b
Adrien BLANCHOT <caboteur.adrienbanchot@gmail.com>
2020-04-21 21:16:02 +0200

    com

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
-
+console.log("ok")
 var countries =  ["ID","HU","HK","GR","FI","EG","DK","KR","CO","CA","CL","BR","AU","AT","AR","SA","ZA","FR","US","DE","GB","IT","BE","IN","IE","IL","JP","KE","MY","MX","NG","NO","NZ","NL","PH","PL","PT","RO","RU","SG","SE","CH","TW","CZ","TH","TR","UA","VN"]
 let nation ="";
 var length = -1;
